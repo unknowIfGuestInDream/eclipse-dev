8a494771dc64d0866205830b493732f9e2f760e8
unknowIfGuestInDream <liang.tang.cx@gmail.com>
2023-10-22 21:18:12 +0800

    feat: 添加示例
    Signed-off-by: unknowIfGuestInDream <liang.tang.cx@gmail.com>

---
+++ examples/eclipse-examples-swt-snippets/src/main/java/module-info.java
@@ -0,0 +1,5 @@
+open module eclipse.examples.swt.snippets {
+    requires org.eclipse.swt;
+    requires org.eclipse.swt.win32.win32.x86_64;
+    requires jogl.all;
+}
---
+++ examples/eclipse-examples-swt/src/main/java/module-info.java
@@ -0,0 +1,4 @@
+open module eclipse.examples.swt {
+    requires org.eclipse.swt;
+    requires org.eclipse.swt.win32.win32.x86_64;
+}
